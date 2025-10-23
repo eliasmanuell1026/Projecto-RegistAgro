cacc77d1e6a85929d931e63c5fe0885e25e1088e
eucleniocadete <eucleniocadete@gmail.com>
2025-10-23 20:29:32 +0100

    Adição do diretório database e remoção do diretório view

--- backend/src/controller/teste.js
+++
@@ -1,7 +0,0 @@
-class Teste {
-    async teste(req, res){
-        res.status(200).json("Hello World!!!")
-    }
-}
-
-export default new Teste()
--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -1,9 +1,6 @@
 import { Router } from "express"
-import Teste from '../controller/teste.js'
 
 const router = Router()
-
-router.get('/', Teste.teste)
 
 
 export default router
