db64c20e07539affcf78d84dc837c193508a46a0
eucleniocadete <eucleniocadete@gmail.com>
2025-10-24 12:51:31 +0100

    Adição de variáveis globais para o servidor e bancos de dados

---
+++ backend/src/config/variaveisGlobais.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+
+const variaveis = {
+    user: process.env.USER,
+    host: process.env.HOST,
+    password: process.env.PASSWORD,
+    database_port: process.env.DATABASE_PORT,
+    database1: process.env.DATABASE1,
+    database2: process.env.DATABASE2,
+    server_port: process.env.SERVER_PORT
+}
+
+export default variaveis
